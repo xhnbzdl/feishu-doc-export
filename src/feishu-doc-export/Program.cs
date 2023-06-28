8c40787946695eaee9f91f44a3cf45e77de08c08
hyx <huangyx1229@163.com>
2023-06-28 14:22:31 +0800

    初始化项目文件

---
+++ src/feishu-doc-export/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace feishu_doc_export
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
