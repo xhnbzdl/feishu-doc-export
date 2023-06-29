bfefa595e8ca75a998648b976ec547e6a8f62cdd
hyx <huangyx1229@163.com>
2023-06-29 11:04:04 +0800

    递归获取知识库下的所有节点信息

--- src/feishu-doc-export/Program.cs
+++ src/feishu-doc-export/Program.cs
@@ -16,16 +16,22 @@ namespace feishu_doc_export
 
             feiShuHttpApi = IOC.IoContainer.GetService<IFeiShuHttpApi>();
 
-            var wikiNodes = GetWikiNodeList("7086342166150250500", parentNodeToken: "wikcnuGeC3ChV3TDglYUkzt7T1c").Result;
+            var wikiNodes = GetAllWikiNode("7086342166150250500").Result;
 
-            wikiNodes = GetWikiChildNodeList("7086342166150250500", wikiNodes).Result;
             Console.WriteLine(wikiNodes.Count);
 
         }
 
-        static async Task<List<WikiNodeItemDto>> GetWikiNodeList(string spaceId, string pageToken = null, string parentNodeToken = null)
+        /// <summary>
+        /// 获取空间子节点列表
+        /// </summary>
+        /// <param name="spaceId">知识空间Id</param>
+        /// <param name="pageToken">分页token，第一次查询没有</param>
+        /// <param name="parentNodeToken">父节点token</param>
+        /// <returns></returns>
+        static async Task<WikiNodePagedResult> GetWikiNodeList(string spaceId, string pageToken = null, string parentNodeToken = null)
         {
-            StringBuilder urlBuilder = new StringBuilder($"{FeiShuConsts.OpenApiEndPoint}/open-apis/wiki/v2/spaces/{spaceId}/nodes?page_size=50");
+            StringBuilder urlBuilder = new StringBuilder($"{FeiShuConsts.OpenApiEndPoint}/open-apis/wiki/v2/spaces/{spaceId}/nodes?page_size=50");// page_size=50
             if (!string.IsNullOrWhiteSpace(pageToken))
             {
                 urlBuilder.Append($"&page_token={pageToken}");
@@ -38,32 +44,73 @@ namespace feishu_doc_export
 
             var resultData = await feiShuHttpApi.GetWikeNodeList(urlBuilder.ToString());
 
-            List<WikiNodeItemDto> nodes = resultData.Data.Items;
+            return resultData.Data;
+        }
 
-            if (resultData.Data.HasMore && !string.IsNullOrWhiteSpace(resultData.Data.PageToken))
+        /// <summary>
+        /// 获取知识空间下全部文档节点
+        /// </summary>
+        /// <param name="spaceId"></param>
+        /// <returns></returns>
+        static async Task<List<WikiNodeItemDto>> GetAllWikiNode(string spaceId)
+        {
+            List<WikiNodeItemDto> nodes = new List<WikiNodeItemDto>();
+            string pageToken = null;
+            bool hasMore;
+            do
             {
-                var moreData = await GetWikiNodeList(spaceId, resultData.Data.PageToken);
-                nodes.AddRange(moreData);
-            }
+                // 分页获取顶级节点，pageToken = null时为获取第一页
+                var pagedResult = await GetWikiNodeList(spaceId, pageToken);
+                nodes.AddRange(pagedResult.Items);
+
+                foreach (var item in pagedResult.Items)
+                {
+                    if (item.HasChild)
+                    {
+                        List<WikiNodeItemDto> childNodes = await GetWikiChildNode(spaceId, item.NodeToken);
+                        nodes.AddRange(childNodes);
+                    }
+                }
+
+                pageToken = pagedResult.PageToken;
+                hasMore = pagedResult.HasMore;
+
+            } while (hasMore && !string.IsNullOrWhiteSpace(pageToken));
 
             return nodes;
         }
 
-        static async Task<List<WikiNodeItemDto>> GetWikiChildNodeList(string spaceId, List<WikiNodeItemDto> wikiNodes)
+        /// <summary>
+        /// 递归获取知识空间下指定节点下的所有子节点（包括孙节点）
+        /// </summary>
+        /// <param name="spaceId">知识空间id</param>
+        /// <param name="parentNodeToken">父节点token</param>
+        /// <returns></returns>
+        static async Task<List<WikiNodeItemDto>> GetWikiChildNode(string spaceId, string parentNodeToken)
         {
-            List<WikiNodeItemDto> newNodes = new List<WikiNodeItemDto>();
-
-            foreach (var item in wikiNodes)
+            List<WikiNodeItemDto> childNodes = new List<WikiNodeItemDto>();
+            string pageToken = null;
+            bool hasMore;
+            do
             {
-                newNodes.Add(item);
-                if (item.HasChild)
+                var pagedResult = await GetWikiNodeList(spaceId, pageToken, parentNodeToken);
+                childNodes.AddRange(pagedResult.Items);
+
+                foreach (var item in pagedResult.Items)
                 {
-                    var childNodes = await GetWikiNodeList(spaceId, parentNodeToken: item.NodeToken);
-                    childNodes.AddRange(await GetWikiChildNodeList(spaceId, childNodes));
-                    newNodes.AddRange(childNodes);
+                    if (item.HasChild)
+                    {
+                        List<WikiNodeItemDto> grandChildNodes = await GetWikiChildNode(spaceId, item.NodeToken);
+                        childNodes.AddRange(grandChildNodes);
+                    }
                 }
-            }
-            return newNodes;
+
+                pageToken = pagedResult.PageToken;
+                hasMore = pagedResult.HasMore;
+
+            } while (hasMore && !string.IsNullOrWhiteSpace(pageToken));
+
+            return childNodes;
         }
     }
 }
