1798fac3bacf7005d054b03deaa8f2d1d3b55e5f
hyx <huangyx1229@163.com>
2023-07-12 16:45:58 +0800

    添加string扩展方法

--- src/feishu-doc-export/Helper/FileHelper.cs
+++ src/feishu-doc-export/Helper/FileHelper.cs
@@ -23,6 +23,20 @@ namespace feishu_doc_export.Helper
         }
 
         /// <summary>
+        /// 保存文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="content"></param>
+        public static Task Save(this string path, string content)
+        {
+            var dir = Path.GetDirectoryName(path);
+
+            dir.CreateIfNotExist();
+
+            return File.WriteAllTextAsync(path, content);
+        }
+
+        /// <summary>
         /// 如果目录不存在，那么创建目录
         /// </summary>
         /// <param name="path"></param>
