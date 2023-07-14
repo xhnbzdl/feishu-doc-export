3b9a0790edd3dc0611d2b53da05e040f9b67354a
hyx <2804184253@qq.com>
2023-07-14 21:26:33 +0800

    调整日志

--- src/feishu-doc-export/Helper/LogHelper.cs
+++ src/feishu-doc-export/Helper/LogHelper.cs
@@ -12,35 +12,37 @@ namespace feishu_doc_export.Helper
         public static void LogInfo(string message)
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"【INFO】：{message}");
+            Console.WriteLine($"【INFO】{message}");
             Console.ResetColor();
         }
 
         public static void LogDebug(string message)
         {
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine($"【DEBUG】：{message}");
+            Console.WriteLine($"【DEBUG】{message}");
             Console.ResetColor();
         }
 
         public static void LogWarn(string message)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"【WARN】：{message}");
+            Console.WriteLine($"【WARN】{message}");
             Console.ResetColor();
         }
 
         public static void LogError(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"【ERROR】：{message}");
+            Console.WriteLine($"【ERROR】{message}（请按任意键退出！）");
+            Console.ReadKey();
+            Environment.Exit(0);
             Console.ResetColor();
         }
 
         public static void LogFatal(string message)
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine($"【FATAL】：{message}");
+            Console.WriteLine($"【FATAL】{message}");
             Console.ResetColor();
         }
     }
